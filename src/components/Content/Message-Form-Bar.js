8810034bd3dfdda8273a806eab480d238a30566b
Davlat Shavkatov <dalisoft@mail.ru>
2019-05-25 10:56:15 +0500

    some minor tweaks

--- src/components/Content/Message-Form-Bar.js
+++ src/components/Content/Message-Form-Bar.js
@@ -4,51 +4,69 @@ import cx from 'classnames';
 import ws from '../../websockets';
 
 class MessageFormBar extends Component {
-	setMessage(messageText) {
+	setMessage = (messageText) => {
 		this.setState({ messageText });
-	}
-	setFieldError(fieldError) {
+	};
+	setFieldError = (fieldError) => {
 		this.setState({ fieldError });
-	}
-	constructor(props) {
-		super(props);
+	};
+	setBtnError = (btnError) => {
+		this.setState({ btnError });
+	};
+	handleMessageInput = (e) => {
+		const { value } = e.target;
 
-		this.state = { messageText: '', fieldError: false };
-	}
-	render() {
-		const { id, actions } = this.props;
-		const { messageText, fieldError } = this.state;
-
-		function handleMessageInput(e) {
-			this.setMessage(e.target.value);
+		if (value.length > 0) {
+			this.setFieldError(false);
 		}
-		function handleMessageSend(e) {
-			e.preventDefault();
-			if (messageText.length > 0) {
-				let socket = ws[id];
-				if (socket) {
-					if (socket.readyState === socket.OPEN) {
-						socket.send(messageText);
-					}
-					else {
-						console.error('Connection was closed before message sent');
-					}
-				}
+		this.setMessage(value);
+	};
+	handleMessagesClear = (e) => {
+		e.preventDefault();
+		const { id, actions } = this.props;
+
+		actions.setConnectionValue(id, 'messages', []);
+	};
+	handleMessageSend = (e) => {
+		e.preventDefault();
+		const { id, actions } = this.props;
+		const { messageText } = this.state;
+		if (messageText.length > 0) {
+			let socket = ws[id];
+			if (socket && socket.readyState === socket.OPEN) {
+				socket.send(messageText);
 				actions.pushMessage(id, 'output', messageText);
 				this.setMessage('');
 				this.setFieldError(false);
+				this.setBtnError(true);
 			}
 			else {
-				this.setFieldError(true);
+				this.setBtnError(true);
 			}
 		}
-
-		if (messageText.length > 0 && fieldError) {
-			this.setFieldError(false);
+		else {
+			this.setFieldError(true);
 		}
+	};
+	constructor(props) {
+		super(props);
+
+		this.state = { messageText: '', btnError: false, fieldError: false };
+	}
+	render() {
+		const { messageText, btnError, fieldError } = this.state;
 
 		return (
-			<form className="app__layout--form app__messages__message--send-bar" onSubmit={handleMessageSend}>
+			<form className="app__layout--form app__messages__message--send-bar" onSubmit={this.handleMessageSend}>
+				<button
+					name="send"
+					key="send-button"
+					type="button"
+					className="app__layout--form-input"
+					onClick={this.handleMessagesClear}
+				>
+					{'Clear Messages'}
+				</button>
 				<input
 					name="message-text"
 					key="message-text-field"
@@ -57,9 +75,16 @@ class MessageFormBar extends Component {
 						'app__layout--form-input-error': fieldError
 					})}
 					placeholder="Type message here"
-					onInput={handleMessageInput}
+					onInput={this.handleMessageInput}
 				/>
-				<button name="send" key="send-button" type="submit" className="app__layout--form-input">
+				<button
+					name="send"
+					key="send-button"
+					type="submit"
+					className={cx('app__layout--form-input', {
+						'app__layout--form-input-error': btnError
+					})}
+				>
 					{'Send'}
 				</button>
 			</form>
